888a037c9abac8550f9cf424b862e580f371b0f4
Игорь Потапов <igor@potapoff.org>
2014-07-09 17:02:07 +0400

    Coins

--- lib/jazz_decimal.ex
+++ lib/jazz_decimal.ex
@@ -3,3 +3,9 @@ defimpl Jazz.Encoder, for: Decimal do
         { Decimal.to_string(dec, :normal) }
     end
 end
+
+defimpl Jazz.Encoder, for: Coins do
+    def to_json(coins, _) do
+        { Coins.to_string(coins) }
+    end
+end
