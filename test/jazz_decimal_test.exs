7a7bd56a679c930ab944afd7ce1919c942cbcb56
Игорь Потапов <igor@potapoff.org>
2014-07-09 14:36:27 +0400

    tests

--- test/jazz_decimal_test.exs
+++ test/jazz_decimal_test.exs
@@ -2,6 +2,8 @@ defmodule JazzDecimalTest do
   use ExUnit.Case
 
   test "the truth" do
-    assert 1 + 1 == 2
+    assert Jazz.encode! Decimal.new(0) == "0"
+    assert Jazz.encode! %{a: Decimal.new(12.1)} == "{\"a\":12.1}"
+    assert Jazz.encode! %{a: Decimal.new("12.1")} == "{\"a\":12.1}"
   end
 end
